3ffb91ee7924f04334f4d93a9d24313f47407ab2
laloops <millalopes.01@gmail.com>
2022-06-25 02:18:37 -0300

    lista de estados e cidades

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import axios from 'axios';
+
 
 const app = express();
 
@@ -16,15 +18,80 @@ app.get('/', (req, res) => {
 });
 
 app.post('/cadastro', (req, res) => {
-res.send(req.body.cidade)
+    res.send(req.body.cidade)
 });
 
-app.get('/ola', (req, res) => {
-    res.send('Olá mundo');
+app.get('/buscaestadoassincrona', (req, res) => {
+    const listaCompleta = [];
+
+    axios.get('http://www.geonames.org/childrenJSON?geonameId=3469034')
+        .then((response) => {
+            const estados = response.data.geonames;
+            for (var i = 0; i < estados.length; i++) {
+                console.log(estados[i].geonameId);
+                var id = estados[i].geonameId;
+                axios.get('http://www.geonames.org/childrenJSON?geonameId=' + id)
+                    .then((responseCidades) => {
+                        const cidades = responseCidades.data.geonames;
+                        for (var j = 0; j < cidades.length; j++) {
+
+                            const salvaDado = {
+                                estado: cidades[j].adminName1,
+                                cidade: cidades[j].name
+                            }
+                            listaCompleta.push(salvaDado)
+                            //salvar salvaDado no mysql
+                            console.log(salvaDado);
+                        }
+                    })
+                    .catch((erro) => {
+                        console.log(erro)
+
+                    })
+            }
+        })
+        .catch((erro) => {
+            console.timeLog(erro)
+
+        })
+
+    res.send(listaCompleta)
 });
 
-app.listen(3000, () => 
-console.log('Servidor iniciado na porta 3000')
+app.get('/buscaestado', async (req, res) => {
+    const listaCompleta = [];
+    try {
+        const response = await axios.get('http://www.geonames.org/childrenJSON?geonameId=3469034')
+        const estados = response.data.geonames;
+
+        for (var i = 0; i < estados.length; i++) {
+            var id = estados[i].geonameId;
+            const responseCidades = await axios.get('http://www.geonames.org/childrenJSON?geonameId=' + id)
+
+            const cidades = responseCidades.data.geonames;
+            for (var j = 0; j < cidades.length; j++) {
+
+                const salvaDado = {
+                    estado: cidades[j].adminName1,
+                    cidade: cidades[j].name
+                }
+                listaCompleta.push(salvaDado)
+                //salvar salvaDado no mysql
+            }
+        }
+        res.send(listaCompleta)
+    } catch (error) {
+        res.status(500).send("erro")
+    }
+});
+
+app.get('/buscacidade/:id', (req, res) => {
+
+});
+
+
+app.listen(3000, () =>
+    console.log('Servidor iniciado na porta 3000')
 );
 
 
