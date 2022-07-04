ba057385c29b44228397c7e2f5f19e673e2d6abb
laloops <millalopes.01@gmail.com>
2022-07-04 18:28:07 -0300

    Criação do menu dropdown

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import { Component } from 'react';
+import React from 'react';
+import Select from 'react-select';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      {/* <img src={logo} className="App-logo" alt="logo" />  */}
-        <p>
-Selecione seu estado:
-        </p><div>
-    
-    </div>
-      </header>
-    </div>
-  );
+const actions = [
+  { label: "Add", value: 1 },
+  { label: "Edit", value: 2 },
+  { label: "Delete", value: 3 }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">Selecione seu estado:
+          </div>
+        </div>
+        <div className="col-md-4">
+        <Select options={ actions } />
+
+          <div className="col-md-4"></div>
+        </div>
+      </div>
+    )
+  };
 }
+
 
 export default App;
